644000bdd14381d4933913c2c01d2701f3584589
Amy Felty <afelty@eecs.uottawa.ca>
2018-04-13 15:34:03 -0400

    progress on Pattern011

--- Pattern011Series.v
+++ Pattern011Series.v
@@ -46,33 +46,43 @@ end.
 Definition Qlt_bool (a b: Q): bool :=
   andb (Qle_bool a b) (negb (Qeq_bool b a)).
 
+(* coercion of binary nats to binary Q *)
+Definition binQ (n:nat) : option Q :=
+match n with
+| 0%nat => (Some 0)
+| 1%nat => (Some 1)
+| _ => None
+end.
 
+(* Input = [0;1;1;...] with indexing starting at 1
+   n mod 3 = 1 -> nth element of Input is 0
+   n mod 3 = 2 -> nth element of Input is 1
+   n mod 3 = 0 -> nth element of Input is 1 *)
 Inductive series2values : nat -> Neuron -> Neuron -> list Q -> list Q -> Prop :=
-| s2v_0 : forall (N1 N2: Neuron) (P1 P2: list Q),
-    nth (Output N1) 0%nat = (Some 0%nat) ->
-    nth (Output N2) 0%nat = (Some 0%nat) ->
-    series2values 0 N1 N2 [0] [0].
-
-(* unfinished part
-| s2v_1 : forall (N1 N2: Neuron) (P1 P2: list Q) (O1 O2 O1' O2':nat) (w1:Q) (t:nat),
-    t = 1 \/ t = 2 ->
-    series2values t N1 N2 P1 P2 ->
-    nth (Output N1) 0%nat = (Some o1%nat) ->
-    nth (Output N2) 0%nat = (Some o2%nat) ->
-    nth (Output N1) 1%nat = (Some o1'%nat) ->
-    nth (Output N2) 1%nat = (Some o2'%nat) ->
-    nth (Weights N1) 0%nat = (Some w1) -> 
-    series2values 1 N1 N2 ((o2%Q*w1)::P1) (..::P2).
-xxx    
-    nth (P1 
-    Output1 = (Qlt_bool 
-    series2values 1 N1 N2 
+| (* This case is before any input is processed *)
+  s2v_start : forall (N1 N2: Neuron) (P1 P2: list Q),
     nth (Output N1) 0%nat = (Some 0%nat) ->
     nth (Output N2) 0%nat = (Some 0%nat) ->
     series2values 0 N1 N2 [0] [0]
-| sv2_mod1 : forall (N1 N2: Neuron) (P1 P2: list Q) (t:nat),
-    t mod 3 = 1%nat \/ ->
-    series2values 0 N1 N2 [0] [0].
-
-               t mod 3 = 1 \/ t mod 3 = 2 *)
+| (* This case processes the next input *)
+  s2v_next : forall (t:nat) (N1 N2: Neuron) (P1 P2: list Q) (Input:Q)
+                      (N1o N2o N1o' N2o':nat) (N1w0 N1w1 N2w:Q) (qN1o qN2o:Q),
+    (((S t) mod 3 = 1%nat /\ Input = 0) \/
+     ((S t) mod 3 = 2%nat /\ Input = 1) \/
+     ((S t) mod 3 = 0%nat /\ Input = 1)) ->
+    (* The following are "known" from the "recursive call" *)
+    series2values t N1 N2 P1 P2 ->
+    nth (Weights N1) 0%nat = (Some N1w0) -> 
+    nth (Weights N1) 1%nat = (Some N1w1) ->
+    nth (Weights N2) 0%nat = (Some N2w) ->
+    nth (Output N1) t = (Some N1o%nat) ->
+    nth (Output N2) t = (Some N2o%nat) ->
+    binQ N1o = (Some qN1o) ->
+    binQ N2o = (Some qN2o) ->
+    (* The following are calculated from the known information *)
+    N1o' = 0%nat -> (* calculation of N1's next output still to be filled in *)
+    N2o' = 0%nat -> (* calculation of N2's next output still to be filled in *)
+    nth (Output N1) (S t) = (Some N1o'%nat) ->
+    nth (Output N2) (S t) = (Some N2o'%nat) ->
+    series2values (S t) N1 N2 (P1 ++ [(qN2o*N1w1)+(Input*N1w0)]) (P2 ++ [qN1o*N2w]).
 
